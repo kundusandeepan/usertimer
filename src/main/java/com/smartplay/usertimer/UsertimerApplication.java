26a704f88ed41aeeefba193b5c8d7ee6cc29a342
Sandeepan Kundu <sandeepan.kundu@gmail.com>
2024-08-29 23:33:03 -0400

    Initial commit

---
+++ src/main/java/com/smartplay/usertimer/UsertimerApplication.java
@@ -0,0 +1,13 @@
+package com.smartplay.usertimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsertimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsertimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartplay/usertimer/UsertimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartplay.usertimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsertimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
